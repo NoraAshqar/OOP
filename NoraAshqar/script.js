2de8f2c47dba8346f59ff9094ebab43f5c3fcae9
Nora Al-Ashqar <noraashqar244@gmail.com>
2025-09-10 10:51:53 +0300

    Update script.js

--- NoraAshqar/script.js
+++ NoraAshqar/script.js
@@ -1,8 +1,13 @@
 function Student(name, age, gradeVal){
   this.name= name;
   this.age= age;
-  let grade= gradeVal;
-
+  let grade;
+  if(gradeVal==="A" ||gradeVal==="F")
+  {
+    grade=gradeVal;
+  }else{
+    console.log("Invalid grade,please enter A or F");
+  }
   this.setGrade= function(newGrade){
     if (newGrade === "A" || newGrade === "F") {
       grade = newGrade;
@@ -12,11 +17,10 @@ function Student(name, age, gradeVal){
   };
   this.getGrade= function () {
     return grade;};
-
-  this.introduce= function(){
-    console.log(`Hi, my name is ${this.name}, I'm ${this.age} years old, and I'm in grade ${grade}.`);
-  };
 }
+Student.prototype.introduce= function(){
+    console.log(`Hi, my name is ${this.name}, I'm ${this.age} years old, and I'm in grade ${this.getGrade()}.`);
+  };
 
 
 const student= new Student("Nora",20,"A");
